bf611b6156dc9abee0bc7e60ab42dd2d1a9f5d2a
DavidMatocha <matochadavid@sssvt.cz>
2026-03-09 22:41:56 +0100

    Předpříprava scriptu

---
+++ script.js
@@ -0,0 +1,5 @@
+const grid = document.getElementById("grid");
+const resetBtn = document.getElementById("resetBtn");
+const selectedColorText = document.getElementById("selectedColorText");
+const colorBoxes = document.querySelectorAll(".color-box");
+
